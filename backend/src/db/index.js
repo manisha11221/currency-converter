5769a968175ef0187cbbb3f1cc455e8d1aa92c98
Manisha Rajput <155047501+manisha11221@users.noreply.github.com>
2025-07-23 12:41:12 +0530

    Update index.js

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 
-const DB_NAME = "currency"
-
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
         console.log(`\n >> MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log(">> MONGODB connection FAILED ", error);
