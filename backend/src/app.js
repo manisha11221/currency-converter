28c42ccd4069798df39b8e923d8e8183c92debe2
Manisha Rajput <155047501+manisha11221@users.noreply.github.com>
2025-07-23 12:16:47 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://currency-converter-ssse.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
